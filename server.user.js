64adfa11f559db847ef64fe2b1e9d2ed74af4a2a
Eric Reed <mrericcoolnessreed@gmail.com>
2021-08-16 20:08:28 -0400

    updated username capitalization in header

--- server.user.js
+++ server.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         YouTube Music Share Server
-// @namespace    https://github.com/An0nDev/ytmusicshare/
+// @namespace    https://github.com/an0ndev/ytmusicshare/
 // @version      0.1
 // @description  lets you share what you're listening to with others
-// @author       An0nDev
+// @author       an0ndev
 // @match        https://music.youtube.com/*
 // @grant        none
 // @require      https://unpkg.com/peerjs@1.3.1/dist/peerjs.min.js
